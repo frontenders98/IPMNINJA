40230340a5985e474da11976c8359dd148aab5c4
frontenders98 <willdexter98@gmail.com>
2025-04-02 23:11:47 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,14 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+    ssl: { rejectUnauthorized: false },
+    connectionTimeoutMillis: 5000,
+    idleTimeoutMillis: 30000
+});
+
+pool.on('error', (err) => {
+    console.error('Unexpected error on idle client:', err.stack);
+    process.exit(-1);
 });
 
 module.exports = { pool };
